e142ffa547171bc04236acbc7865d0a52f636d2c
Saif <saifeldein0720@gmail.com>
2023-07-06 21:40:54 +0300

    firebase integration + dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 
 // testttttt
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
