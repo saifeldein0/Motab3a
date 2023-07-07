27f40ee938e53323e5cec4550f8adcb50401ea7c
rawannmmohamed <76457482+rawannmmohamed@users.noreply.github.com>
2023-07-07 23:31:25 +0300

    r

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
