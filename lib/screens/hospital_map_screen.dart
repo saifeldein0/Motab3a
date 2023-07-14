065d3e30c64c3234c1f949399a4c76f908202d78
Saif <saifeldein0720@gmail.com>
2023-07-14 15:42:55 +0300

    register edits, hospital map screen

---
+++ lib/screens/hospital_map_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+class HospitalMapScreen extends StatelessWidget {
+  const HospitalMapScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hospital Map'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Launch Google Maps
+            launchMaps();
+          },
+          child: const Text('Find Nearest Hospitals'),
+        ),
+      ),
+    );
+  }
+
+  void launchMaps() async {
+    // URL scheme for launching Google Maps
+    final String googleMapsUrl = 'https://www.google.com/maps/search/?api=1&query=hospitals';
+
+    if (await canLaunch(googleMapsUrl)) {
+      await launch(googleMapsUrl);
+    } else {
+      throw 'Could not launch $googleMapsUrl';
+    }
+  }
+}
