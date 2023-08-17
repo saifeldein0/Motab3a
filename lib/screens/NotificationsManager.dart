d65c58ac62709d5a3750bbf592f7a4a0a57616c9
Saif <saifeldein0720@gmail.com>
2023-08-17 20:37:45 +0300

    test

--- lib/screens/NotificationsManager.dart
+++ lib/screens/NotificationsManager.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
@@ -103,3 +103,4 @@ class DailyNotification {
     required this.time,
   });
 }
+*/
